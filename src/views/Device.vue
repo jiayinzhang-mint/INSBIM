2fd8b141ffb30acf223b97d6c066b19c95fe4d56
MINT <mint@insdim.com>
2019-04-28 09:39:40 +0800

    device page

--- src/views/Device.vue
+++ src/views/Device.vue
@@ -1,34 +1,18 @@
 <template>
   <v-container grid-list-md>
-    <v-layout row wrap>
-      <v-flex xs2>
-        <v-navigation-drawer permanent stateless value="true" class="transparent">
-          <v-list>
-            <template v-for="(item,j) in items">
-              <v-list-tile :key="j" :to="item.route" ripple>
-                <v-list-tile-content>
-                  <v-list-tile-title
-                    v-if="item.heading"
-                    class="text-xs-right pr-3 grey--text body-1 font-weight-medium"
-                  >{{ item.heading }}</v-list-tile-title>
-                  <v-list-tile-title v-else class="text-xs-right pr-3">{{ item.title }}</v-list-tile-title>
-                </v-list-tile-content>
-              </v-list-tile>
-            </template>
-          </v-list>
-        </v-navigation-drawer>
-      </v-flex>
-      <v-flex xs10>
-        <v-card>
-          <router-view></router-view>
-        </v-card>
-      </v-flex>
-    </v-layout>
+    <v-card>
+      <device-manage></device-manage>
+    </v-card>
   </v-container>
 </template>
 
 <script>
+import deviceManage from "../components/device/SensorManage";
+import device from "../store/modules/device";
 export default {
+  components: {
+    deviceManage: deviceManage
+  },
   data() {
     return {
       items: [
